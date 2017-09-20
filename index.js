f2cf7a3799a2790837c7d16555700d28e0295b2c
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-20 22:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function theBeatlesPlay(musicians, instruments){
   }
   return newArr;
 }
+
+function johnLennonFacts(facts){
+  var i=0;
+  while(i<facts.length){
+    facts[i] = `${facts[i]} !!!`;
+  }
+  return facts;
+}
